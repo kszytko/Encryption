aed4a37a13d6e7762a6e0479c9435e3e952bc7cf
kszytko <>
2020-03-29 16:41:24 +0200

    Comment Update

--- Encryption/Encryption.cpp
+++ Encryption/Encryption.cpp
@@ -1,4 +1,25 @@
-﻿#include <string> 
+﻿/*
+    K.Szytko 29.03.2020
+
+    Generowanie klucza polega na stworzeniu wektora kolejnych 95 znaków
+    drukowanych (w tym spacji) z pomocą funkcji iota, przemieszanie go
+    funkcją shuffle i zwrócenie w postacji stringa.
+
+    Wektor iota:    " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMN
+                    OPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+    Przykładowy     "QE*5mh{,Vp!tw\\qxLZ6e$Yoj@\"X}d4^AbSIskv/O7%>yfP
+    wektor shuffle: unCTc8?#N=l<H)g(.`:;~93DiB1WJ|KF+]rR &[_G20zU'M-a"
+
+    Szyfrowanie znaku polega na wyliczeniu jego pozycji i odczytaniu znaku z klucza.
+    Odszyfrowanie jest zadaniem odwrotnym i po znalezieniu znaku w kluczu odczytywana jest
+    jego pozycja, na podstawie której wyliczany jest znak ASCII.
+
+    Znaki specjalne m.in. znak nowej lini nie są modyfikowane.
+*/
+
+
+#include <string> 
 #include <iostream> 
 #include <cassert>
 #include <vector>
@@ -6,21 +27,6 @@
 #include <random>
 #include <algorithm>
 
-/* 
-Generowanie klucza polega na stworzeniu wektora kolejnych 95 znaków 
-drukowanych (w tym spacji) z pomocą funkcji iota, przemieszanie go 
-funkcją shuffle i zwrócenie w postacji stringa.
-
-Wektor iota:    " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMN
-                OPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-
-Wektor shuffle: "QE*5mh{,Vp!tw\\qxLZ6e$Yoj@\"X}d4^AbSIskv/O7%>yfP
-                unCTc8?#N=l<H)g(.`:;~93DiB1WJ|KF+]rR &[_G20zU'M-a"
-
-
-
-*/
- 
 const short ASCII_BEGIN = 32; //Include (Space)
 const short ASCII_END = 127;  //Exclude (DEL)
 
