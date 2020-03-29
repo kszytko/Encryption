8bd9563002bae6b4681f568f17e1c5a18d30c755
kszytko <krzysiek.szytko@gmail.com>
2020-03-29 15:56:05 +0200

    First Commit

---
+++ Encryption/Encryption.cpp
@@ -0,0 +1,61 @@
+﻿#include <string> 
+#include <iostream> 
+#include <cassert>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <algorithm>
+
+/* 
+Generowanie klucza polega na stworzeniu wektora kolejnych 95 znaków 
+drukowanych (w tym spacji) z pomocą funkcji iota, przemieszanie go 
+funkcją shuffle i zwrócenie w postacji stringa.
+
+Wektor iota:    " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMN
+                OPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+Wektor shuffle: "QE*5mh{,Vp!tw\\qxLZ6e$Yoj@\"X}d4^AbSIskv/O7%>yfP
+                unCTc8?#N=l<H)g(.`:;~93DiB1WJ|KF+]rR &[_G20zU'M-a"
+
+
+
+*/
+ 
+const short ASCII_BEGIN = 32; //Include (Space)
+const short ASCII_END = 127;  //Exclude (DEL)
+
+std::string generateKey() {
+    std::vector<char> vKey(ASCII_END - ASCII_BEGIN);
+    std::iota(vKey.begin(), vKey.end(), ASCII_BEGIN);
+    std::shuffle(vKey.begin(), vKey.end(), std::mt19937{ std::random_device{}() });
+    return std::string{ vKey.begin(), vKey.end() };
+}
+
+std::string encrypt(std::string m, std::string key) {
+    std::transform(m.begin(), m.end(), m.begin(), 
+        [&key](char c) {
+            return (c >= ASCII_BEGIN && c < ASCII_END) ? key[c - ASCII_BEGIN] : c; });
+    return m;
+}
+
+std::string decrypt(std::string m, std::string key) {
+    std::transform(m.begin(), m.end(), m.begin(),
+        [&key](char c) {
+            return (c >= ASCII_BEGIN && c < ASCII_END) ? key.find(c) + ASCII_BEGIN : c; });
+    return m;
+}
+
+int main() {
+    std::string message = "Hello,\tCoders School!";
+
+    auto key = generateKey();
+    auto cypher = encrypt(message, key);
+    auto result = decrypt(cypher, key);
+
+    std::cout << "Message: " << message << '\n';
+    std::cout << "Cypher : " << cypher << '\n';
+    std::cout << "Result : " << result << '\n';
+
+    assert(message == result);
+    return 0;
+}
